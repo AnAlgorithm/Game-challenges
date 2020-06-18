94395c6438b16301dbfec29a8bd55523c3061539
AnAlgorithm <64064699+AnAlgorithm@users.noreply.github.com>
2020-06-18 16:44:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 print "Game Challenge"
 import math
 import time
+import random
+lst=["What is 7+7?"]
+x=random.randint(1,27)
+for i in range(9):
+  ls=str(input(lst[x]))
+  if at[x]=ls:
+    print "That was correct!
+  else:
+    print "That was not correct!"
